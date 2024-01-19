f14faaf5e2596c76bb9660cef93343bc8933bb6e
Tamer Erdoğan <tamer.erdogaan@gmail.com>
2024-01-19 04:31:52 +0300

    LoanBookAction action.

--- src/Manager/LibraryManager.cs
+++ src/Manager/LibraryManager.cs
@@ -54,6 +54,29 @@ public class LibraryManager
     public bool LoanBookAction()
     {
         Console.Clear();
+        Helper.DrawTable(books);
+        Console.WriteLine("\nÖdünç alınacak kitabın ISBN numarasını giriniz:");
+        string isbn = Console.ReadLine() ?? "";
+        Book? findedBook = books.Find(book => book.Isbn == isbn);
+
+        Console.WriteLine("");
+        if (findedBook == null)
+        {
+            Console.WriteLine("ISBN bulunamadı.");
+        }
+        else
+        {
+            if (findedBook.AvailableCount() > 0)
+            {
+                findedBook.DueDates.Add(DateTime.Now.AddDays(7));
+                Console.WriteLine("Ödünç işlemi başarılı.");
+            }
+            else
+            {
+                Console.WriteLine("Bu kitap için boşta kopya bulunmamakta.");
+            }
+        }
+
         WaitBeforeGoBack();
         return true;
     }
