a00af8d56d870bf62b0aeddbc7fd4aafbf027896
Tamer Erdoğan <tamer.erdogaan@gmail.com>
2024-01-19 04:26:04 +0300

    SearchBookAction done.

--- src/Manager/LibraryManager.cs
+++ src/Manager/LibraryManager.cs
@@ -30,6 +30,23 @@ public class LibraryManager
     public bool SearchBookAction()
     {
         Console.Clear();
+        Console.Write("Arama yapmak için kitap yada yazar adı giriniz: ");
+        string searchKey = Console.ReadLine() ?? "";
+        List<Book> findedBooks = books.FindAll(book =>
+            book.Title.Contains(searchKey, StringComparison.OrdinalIgnoreCase)
+            || book.Author.Contains(searchKey, StringComparison.OrdinalIgnoreCase)
+        );
+
+        Console.WriteLine("");
+        if (findedBooks.Count == 0)
+        {
+            Console.WriteLine("Kitap bulunamadı.");
+        }
+        else
+        {
+            Helper.DrawTable(findedBooks);
+        }
+
         WaitBeforeGoBack();
         return true;
     }
